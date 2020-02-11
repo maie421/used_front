a143e9b4fbe459a44e84b54ca8fea12bc46d3175
윤영미 <maie421@naver.com>
2020-02-12 02:59:21 +0900

    preload

--- App.js
+++ App.js
@@ -1,19 +1,32 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState, useEffect } from "react";
+import { Ionicons } from "@expo/vector-icons";
+import { AppLoading} from "expo";
+import {Asset} from 'expo-asset';
+import * as Font from 'expo-font';
+import { Text, View } from "react-native";
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>성공하였습다</Text>
+  const [loaded, setLoaded] = useState(false);
+  const preLoad = async () => {
+    try {
+      await Font.loadAsync({
+        ...Ionicons.font
+      });
+      await Asset.loadAsync([require("./assets/logo.png")]);
+      console.log("시작");
+      setLoaded(true);
+    } catch (e) {
+      console.log(e);
+    }
+  };
+  useEffect(() => {
+    preLoad();
+  }, []);
+  return loaded ? (
+    <View>
+      <Text>Open up App.js to start working on your app!</Text>
     </View>
+  ) : (
+    <AppLoading />
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
