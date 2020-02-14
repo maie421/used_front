4eed0c7a6c73699cfdc38faae1817ce2807ef795
윤영미 <maie421@naver.com>
2020-02-15 01:41:29 +0900

    Tabs Navigation

--- App.js
+++ App.js
@@ -55,7 +55,7 @@ export default function App() {
   return loaded && client && isLoggedIn !== null ? (
     <ApolloProvider client={client}>
       <ThemeProvider theme={styles}>
-      <AuthProvider isLoggedIn={isLoggedIn}>
+        <AuthProvider isLoggedIn={isLoggedIn}>
           <NavController />
         </AuthProvider>
       </ThemeProvider>
