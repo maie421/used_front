749ecbeb1133a18f79d3304da0798a2e6053475f
윤영미 <maie421@naver.com>
2020-02-19 22:49:46 +0900

    login 수정

--- components/AuthInput.js
+++ components/AuthInput.js
@@ -1,3 +1,4 @@
+
 import React from "react";
 import styled from "styled-components";
 import PropTypes from "prop-types";
@@ -8,7 +9,7 @@ const Container = styled.View`
 `;
 
 const TextInput = styled.TextInput`
-  width: ${constants.width / 2};
+  width: ${constants.width / 1.7};
   padding: 10px;
   background-color: ${props => props.theme.greyColor};
   border: 0.5px solid ${props => props.theme.darkGreyColor};
@@ -22,23 +23,23 @@ const AuthInput = ({
   autoCapitalize = "none",
   returnKeyType = "done",
   onChange,
-  onEndEditing = () => null,
+  onSubmitEditing = () => null,
   autoCorrect = true
-  
 }) => (
   <Container>
     <TextInput
-      onChange={onChange}
+      onChangeText={onChange}
       keyboardType={keyboardType}
       returnKeyType={returnKeyType}
       placeholder={placeholder}
       autoCapitalize={autoCapitalize}
-      onEndEditing={onEndEditing}
+      onSubmitEditing={onSubmitEditing}
       autoCorrect={autoCorrect}
       value={value}
     />
   </Container>
 );
+
 AuthInput.propTypes = {
   placeholder: PropTypes.string.isRequired,
   value: PropTypes.string.isRequired,
@@ -50,72 +51,11 @@ AuthInput.propTypes = {
     "email-address",
     "phone-pad"
   ]),
-  
-    autoCapitalize: PropTypes.oneOf(["none", "sentences", "words", "characters"]),
-    onChange: PropTypes.func.isRequired,
-    returnKeyType: PropTypes.oneOf(["done", "go", "next", "search", "send"]),
-    onEndEditing: PropTypes.func,
-    autoCorrect: PropTypes.bool
+  autoCapitalize: PropTypes.oneOf(["none", "sentences", "words", "characters"]),
+  onChange: PropTypes.func.isRequired,
+  returnKeyType: PropTypes.oneOf(["done", "go", "next", "search", "send"]),
+  onSubmitEditing: PropTypes.func,
+  autoCorrect: PropTypes.bool
 };
 
 export default AuthInput;
-// import React from "react";
-// import styled from "styled-components";
-// import PropTypes from "prop-types";
-// import constants from "../constants";
-
-// const Container = styled.View`
-//   margin-bottom: 10px;
-// `;
-
-// const TextInput = styled.TextInput`
-//   width: ${constants.width / 1.7};
-//   padding: 10px;
-//   background-color: ${props => props.theme.greyColor};
-//   border: 0.5px solid ${props => props.theme.darkGreyColor};
-//   border-radius: 4px;
-// `;
-
-// const AuthInput = ({
-//   placeholder,
-//   value,
-//   keyboardType = "default",
-//   autoCapitalize = "none",
-//   returnKeyType = "done",
-//   onChange,
-//   onSubmitEditing = () => null,
-//   autoCorrect = true
-// }) => (
-//   <Container>
-//     <TextInput
-//       onChangeText={onChange}
-//       keyboardType={keyboardType}
-//       returnKeyType={returnKeyType}
-//       placeholder={placeholder}
-//       autoCapitalize={autoCapitalize}
-//       onSubmitEditing={onSubmitEditing}
-//       autoCorrect={autoCorrect}
-//       value={value}
-//     />
-//   </Container>
-// );
-
-// AuthInput.propTypes = {
-//   placeholder: PropTypes.string.isRequired,
-//   value: PropTypes.string.isRequired,
-//   keyboardType: PropTypes.oneOf([
-//     "default",
-//     "number-pad",
-//     "decimal-pad",
-//     "numeric",
-//     "email-address",
-//     "phone-pad"
-//   ]),
-//   autoCapitalize: PropTypes.oneOf(["none", "sentences", "words", "characters"]),
-//   onChange: PropTypes.func.isRequired,
-//   returnKeyType: PropTypes.oneOf(["done", "go", "next", "search", "send"]),
-//   onSubmitEditing: PropTypes.func,
-//   autoCorrect: PropTypes.bool
-// };
-
-// export default AuthInput;
